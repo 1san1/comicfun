ef38ea1f3c89b35a7f18088ab9a28972a2287fb2
ThePlugJumbo <chnxcx@gmail.com>
2022-12-24 15:44:00 -0800

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+    <Component {...pageProps} />
+    <Analytics />  
+  </div>
 }
 
 export default MyApp
